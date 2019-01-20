ffd91d1f4f1a95af606e29bd89da9a5275cf8216
Moses Olawoyin <m.bastford@gmail.com>
2019-01-20 15:48:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma ( phrase = 'I LOVE YOU GRANDMA!')
   
   if phrase = 'I LOVE YOU GRANDMA!'
     
-     puts 'I LOVE YOU TOO SUNNY!'
+     puts 'I LOVE YOU TOO PUMPKIN!'
 
     
     elsif phrase.downcase
