0c544e31e80240beeaee83060f876af82193901c
Moses Olawoyin <m.bastford@gmail.com>
2019-01-20 15:35:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,16 +14,15 @@
 
 
 def speak_to_grandma ('I LOVE YOU GRANDMA!')
+  
   if speak_to_grandma == 'I LOVE YOU GRANDMA!'
-    
-    puts 'I LOVE YOU TOO SUNNY!'
-    
-  end
+     puts 'I LOVE YOU TOO SUNNY!'
+
     
     elsif speak_to_grandma.downcase
     puts 'HUH?! SPEAK UP, SONNY!'
   
     else speak_to_grandma.upcase
     puts 'NO, NOT SINCE 1938!'
-  end 
+
 end
